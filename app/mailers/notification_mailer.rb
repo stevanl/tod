19d34207be9988f84634aeab68f97d82ecacac0d
Stevan Litobac <stevan@litobac.com>
2014-08-21 21:36:20 +0200

    [MAILER] Update subject in mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,14 +2,14 @@ class NotificationMailer < ActionMailer::Base
   default from: "Stevan Litobac <stevan@independentfx.com>"
   def brochure_email(person)
     @person = person
-    mail(:to => ['mark@jam-events.com', 'desi@jam-events.com'], :bcc => 'stevan@mycurrencytransfer.com', :subject => "BYPS: New Brochure Order Form")
+    mail(:to => ['mark@jam-events.com', 'desi@jam-events.com'], :bcc => 'stevan@mycurrencytransfer.com', :subject => "OneDinner: New Brochure Order Form")
   end
   def reply_email(person)
     @person = person
-    mail(:to => ['mark@jam-events.com', 'desi@jam-events.com'], :bcc => 'stevan@mycurrencytransfer.com', :subject => "BYPS: New Reply Submitted")
+    mail(:to => ['mark@jam-events.com', 'desi@jam-events.com'], :bcc => 'stevan@mycurrencytransfer.com', :subject => "OneDinner: New Reply Submitted")
   end
   def dedication_email(person)
     @person = person
-    mail(:to => ['mark@jam-events.com', 'desi@jam-events.com'], :bcc => 'stevan@mycurrencytransfer.com', :subject => "BYPS: New Dedication Submitted")
+    mail(:to => ['mark@jam-events.com', 'desi@jam-events.com'], :bcc => 'stevan@mycurrencytransfer.com', :subject => "OneDinner: New Dedication Submitted")
   end
 end
